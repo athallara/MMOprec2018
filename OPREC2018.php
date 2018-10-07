9e30538c23d3d4c8c004e5df0ff9b688fdce6fa9
AthallaR <30741314+athallara@users.noreply.github.com>
2018-10-07 15:21:54 +0700

    Update OPREC2018.php

--- OPREC2018.php
+++ OPREC2018.php
@@ -55,5 +55,28 @@ echo $angkatan = substr($nim,5,1) .' : menyatakan kode angkatan <br>';
 echo $mahasiswa = substr($nim,6) .' : menyatakan kode nomor mahasiswa <br>';
 
 
+/*---------------------------------------
+
+SOAL TAMBAHAN : MENAMPILKAN NILAI TERKECIL DARI MATRIKS 5X5
+
+----------------------------------------*/
+
+$matrix= array(
+            array(1,2,3,4,5),       //angka terkecil 1
+            array(6,7,8,9,10),
+            array(11,12,13,14,15),
+            array(16,17,18,19,20),
+            array(21,22,23,24,25)   //angka terbesar 25
+        );
+$col = count($matrix);
+
+$result = array();
+for($i=0;$i<$col;$i++){        
+	for($j=0;$j<$col;$j++){         
+            array_push($result,$matrix[$i][$j]); //Memasukan array values kedalam $result
+	}   	
+}
+
+print_r(min($result)); //Menampilkan values terkecil dari array $result  // ubah 'min' --> 'max' untuk tampilkan angka terbesar
 
 ?>
